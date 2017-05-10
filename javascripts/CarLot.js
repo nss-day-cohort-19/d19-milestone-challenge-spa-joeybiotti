89547c1dd48728d5c7d949a1debbf2d7076efc87
Joey <joeybb@MacBook-Pro.local>
2017-05-10 15:57:02 -0500

    first commit-- json complete & valid

---
+++ javascripts/CarLot.js
@@ -0,0 +1,18 @@
+"user strict";
+
+console.log("CarLot.js");
+
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+
+      });
+    }
+  };
+
+})();
